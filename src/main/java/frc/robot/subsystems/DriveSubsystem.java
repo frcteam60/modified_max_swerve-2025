a6974639cd8950d903a351e3db083e2ecc0b0aa6
Team 60 <firstteam60robotics@gmail.com>
2025-02-10 20:00:48 -0700

    auto drive mostly straight now

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -160,14 +160,14 @@ public class DriveSubsystem extends SubsystemBase {
         (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
         new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
                 new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-                new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+                new PIDConstants(2.5, 0.0, 0.0) // Rotation PID constants
         ),
         config, // The robot configuration
         () -> {
           // Boolean supplier that controls when the path will be mirrored for the red alliance
           // This will flip the path being followed to the red side of the field.
           // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
+          //TODO field side probally needs to be in auto init instead not when we turn on?
           var alliance = DriverStation.getAlliance();
           if (alliance.isPresent()) {
             return alliance.get() == DriverStation.Alliance.Red;
